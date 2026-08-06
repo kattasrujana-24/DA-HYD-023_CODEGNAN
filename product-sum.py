ea249a5607af4d5b5693f70d5b77ef6060225e84
kattasrujana-24 <kattasrujana569@gmail.com>
2026-08-06 13:04:21 +0530

    usingforloop

---
+++ product-sum.py
@@ -0,0 +1,55 @@
+'''
+#sum of given numbers using for loop
+price =
+list(map(int,input().split(',')))
+total = 0
+for i in price:
+    total = total + i
+print(total)
+'''
+'''
+#password analyzer - upper case,lower case,digits,special characters
+password = "Srujana@0324"
+print(password)
+a = "Srujana@0324"
+upper = 0
+lower = 0
+digits =0
+special = 0
+for ch in password:
+    if 'A'<= ch <='Z':
+        upper +=1
+    elif 'a'<= ch <='z':
+        lower +=1
+    elif '0'<= ch <='9':
+        digits +=1
+else:
+    special +=1
+print("upper_case",upper)
+print("lower_case",lower)
+print("digits",digits)
+print("special_chr",special)
+   
+#change the domain name
+email = input().split()
+for mail in email:
+    print(mail.split('@')[1])
+'''    
+#movies should return with index
+movie = (int,input("Enter the movie name:"))
+for i in movie:
+    print(movie.split('')[1])
+
+
+
+
+
+
+
+
+
+
+
+
+
+

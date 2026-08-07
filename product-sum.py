d81b09051de439a4173f40efbe7f805cccc4a406
kattasrujana-24 <kattasrujana569@gmail.com>
2026-08-07 12:43:42 +0530

    usingforwhile-practice"product-sum.py

--- product-sum.py
+++ product-sum.py
@@ -51,7 +51,7 @@ for i in range(n):
   c = a+b
   a = b
   b = c
-  '''
+
 #fibonacci with while loop
 n = int(input("Enter the numbers:"))
 a,b = 0,1
@@ -62,6 +62,54 @@ while i<n:
     a = b
     b = c
     i+=1
+    
+#write a program to calculate the innings of a batsman and count the boundaries,dotballs and total score 
+#using for loop
+runs = [4,6,1,0,2,4,0,6]
+total_score = boundaries = dotballs = 0
+for i in runs:
+    total_score = total_score + i
+    if i == 4:
+        boundaries = boundaries + 1
+    elif i == 0:
+        dotballs = dotballs + 1
+print("total_score",total_score)
+print("boundaries",boundaries)
+print("dotballs",dotballs)
+'''
+#phonelock pattern using while loop
+pin = "2003"
+max_attempts = 5
+current_attempt = 0
+while current_attempt < max_attempts:
+   entered_pin = input("Enter the pin:")
+   if entered_pin == pin:
+     print("lock_opened")
+     break
+   else:
+      print("entered pin is wrong")
+      current_attempt +=1
+else:
+    print("phone is locked")
+    
+#atm verification
+pin = "2003"
+max_attempts = 3
+current_attempt = 0
+while current_attempt < max_attempts:
+   entered_pin = input("Enter the pin:")
+   if entered_pin == pin:
+     print("lock_opened")
+     break
+   else:
+      print("entered pin is wrong")
+      current_attempt +=1
+else:
+    print("acount is locked")    
+    
+   
+        
+        
 
   
 

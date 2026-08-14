f88b4eb9d83ba5392482bea8799596492665bbac
kattasrujana-24 <kattasrujana569@gmail.com>
2026-08-14 21:19:32 +0530

    task-10

---
+++ day10.py-task2.py
@@ -0,0 +1,11 @@
+#data = ['codegnan','saketh','python','java'] #input
+#output should be as follows
+'''
+0 : codegnan
+1 : saketh
+2 : python
+3 : java
+'''
+data = ['codegnan','saketh','python','java']
+for i in range(len(data)):
+    print(i,":",data[i])

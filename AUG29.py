057ca17b97891d886b23b70486ebbc2b380388e2
kattasrujana-24 <kattasrujana-24>
2026-09-03 06:50:32 +0000

    modulesusage

---
+++ AUG29.py
@@ -0,0 +1,16 @@
+'''
+POP --> Functions(**args & **kwargs), Execption Handling
+OOP --> Inheritance, Polymorphism, Encapsulation, Abstraction
+Modules --> A Module is a python file containing variable, funcitons, objects -->import keyword will be used...
+'''
+def greeting():
+    """Simple Greeting Function"""
+    return "Welocome to Codegnan"
+print(greeting())
+
+details = {'company':'Codegnan','batch':'DA-23','place':'Hyd'}
+
+if __name__ == "__main__":
+    print(greeting())
+print(__name__)
+print(greeting())
